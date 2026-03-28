4696b0890873d017c30fc45bc7f5a4ada861ba98
achcyano <6617260@qq.com>
2026-03-28 18:43:10 +0800

    fix: 清理遗留待办旧代码并更新项目说明

--- lib/core/const.dart
+++ lib/core/const.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:todolist/utils/preferences.dart';
@@ -12,42 +11,10 @@ Future<int> getAppVersionCode() async {
 
 Future<void> checkUpdate() async {}
 
-class Todo {
-  final String title;
-  final bool done;
-  final int? weekday;
-  final TimeOfDay? time;
-
-  Todo({required this.title, required this.done, this.weekday, this.time});
-
-  Map<String, dynamic> toJson() => {
-    'title': title,
-    'done': done,
-    'weekday': weekday,
-    'hour': time?.hour,
-    'minute': time?.minute,
-  };
-
-  factory Todo.fromJson(Map<String, dynamic> json) {
-    return Todo(
-      title: json['title'] as String,
-      done: json['done'] as bool? ?? false,
-      weekday: json['weekday'] as int?,
-      time: (json['hour'] != null && json['minute'] != null)
-          ? TimeOfDay(hour: json['hour'] as int, minute: json['minute'] as int)
-          : null,
-    );
-  }
-}
-
 class Const {
   static final githubUrl =
       'https://github.com/hippoangaiun-art/todolist_flutter';
 
-  static final todoLists = PrefField<String>(
-    'todolist',
-    '[{"title":"Welcome","done":false,"weekday":1,"hour":9,"minute":0}]',
-  );
   static final todoListV2 = PrefField<String>('todos_v2', '[]');
   static final scheduleCourses = PrefField<String>('schedule_courses_v1', '[]');
   static final scheduleSections = PrefField<String>(
